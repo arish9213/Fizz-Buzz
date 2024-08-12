efbce40a51d96bd340a1e829e039534ce5966ded
arish9213 <ariamas2021@gmail.com>
2024-08-12 19:18:01 -0230

    erased "number" for final product

--- Fizz_buzz.py
+++ Fizz_buzz.py
@@ -2,16 +2,21 @@
 i = 1
 
 #start while loop
-while i <= 100:
+while i in range(100):
     print(i)
     i = i + 1
 
 #add if divisible by 3 and 5
     if i % 3 == 0 and i % 5 == 0:
+        i = i +1
         print("FizzBuzz")
+
 #add if divisible by 3
     elif i % 3 == 0:
+        i = i + 1
         print("Fizz")
+
 #add if divisble by 5
     elif i % 5 == 0:
         print("Buzz")
+        i = i + 1
