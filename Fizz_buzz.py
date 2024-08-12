8bade6abd32b3f075e7aed349c55397d4624b463
arish9213 <ariamas2021@gmail.com>
2024-08-12 19:23:45 -0230

    fixed to add else

--- Fizz_buzz.py
+++ Fizz_buzz.py
@@ -3,9 +3,7 @@ i = 1
 
 #start while loop
 while i in range(100):
-    print(i)
-    i = i + 1
-
+    
 #add if divisible by 3 and 5
     if i % 3 == 0 and i % 5 == 0:
         i = i +1
@@ -19,4 +17,8 @@ while i in range(100):
 #add if divisble by 5
     elif i % 5 == 0:
         print("Buzz")
+        i = i + 1
+
+    else:    
+        print(i)
         i = i + 1
